0ce222b9980edcab5c6c4f357d8ad01582aa89d8
Yashvi Chauhan <yashvi.chauhan1909@gmail.com>
2025-10-27 22:45:55 +0530

    lab7

--- factfnc.c
+++ factfnc.c
@@ -3,27 +3,32 @@
 //Otherwise, FACT(n) = n * FACT(n-1). Using this function, WAP to compute the binomial coefficient. 
 //Tabulate the results for different values of n and r with suitable messages.
 #include <stdio.h>
-int f=1;
+/*int f=1;
 int fact(int n){
-    if (n==0){
-        return 1;
-    }
-    else{
     for(int i=1; i<=n; i++){
         f= f*i;
     }
-    return 0;
-    }
+    return f;
 }
 int main(){
     int n;
     printf("enter number: ");
     scanf("%d", &n);
-    int result= fact(n);
-    if (result==0){
-        printf("factorial is %d",f);
+    printf("factorial is %d",f);
+    return 0;
+}*/
+int fact(int n){
+    if (n==0){
+        return 1;
     }
-    else {
-        printf("factorial is 1");
+    else{
+        return n*fact(n-1);
     }
+}
+int main(){
+    int n, result;
+    printf("enter number: ");
+    scanf("%d", &n);
+    printf("factorial is %d",fact(n));
+    return 0;
 }
