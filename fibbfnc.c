e19beca742589b29d4880334b0a00ac1bfbe95e1
Yashvi Chauhan <yashvi.chauhan1909@gmail.com>
2025-11-30 17:45:06 +0530

    practice

--- fibbfnc.c
+++ fibbfnc.c
@@ -1,25 +1,23 @@
 //Develop a recursive function FIBO (num) that accepts an integer argument. 
 //Write a C program that invokes this function to generate the Fibonacci sequence up to num. 
-#include<stdio.h>
-int fibo(int num){
-    if (num==0){
+#include <stdio.h>
+int fibo(int n){
+    if(n==0){
         return 0;
     }
-    else if(num==1){
+    else if(n==1){
         return 1;
     }
     else{
-        return fibo(num-1)+fibo(num-2);
+        return fibo(n-1)+fibo(n-2);
     }
 }
 int main(){
-    int n, i;
-    printf("enter the num of terms: ");
+    int n;
+    printf("enter number of terms: ");
     scanf("%d", &n);
-    printf("fibonacci sequence: ");
-    for(i=0; i<n; i++){
+    for(int i=0; i<n; i++){
         printf("%d ", fibo(i));
     }
-    printf("\n");
-    return 0;
+    return 0; 
 }
